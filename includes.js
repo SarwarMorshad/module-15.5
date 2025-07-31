37e15dba72fd0a9b75aef46cc4f2e27ea15fe467
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-07-31 10:44:27 +0200

    Array push pop shift isArray.........

---
+++ includes.js
@@ -0,0 +1,6 @@
+const friends = ["Alice", "Bob", "Charlie", "David", "Eve"];
+
+console.log(friends.includes("Alice"));
+console.log(friends.indexOf("Charlie"));
+
+console.log(Array.isArray(friends)); // Check if friends is an array
--- pushpop.js
+++ pushpop.js
@@ -2,4 +2,9 @@ const ages = [];
 const numbers = [1, 2, 3, 4, 5];
 numbers.push(6); // Add 6 to the end
 numbers.push(7, 8, 9); // Add 7, 8, and 9 to the end
-console.log(numbers);
+// console.log(numbers);
+
+const friends = ["Alice", "Bob", "Charlie", "David", "Eve"];
+friends.push("Frank"); // Add "Frank" to the end
+friends.push("Grace", "Hannah"); // Add "Grace" and "Hannah"
+// console.log(friends);
