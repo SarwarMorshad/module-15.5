9495823a69990d9789c32d52e96fc236f5465643
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-07-31 09:54:12 +0200

    Array practice

---
+++ array.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 4, 5];
+const friends = ["Alice", "Bob", "Charlie"];
+const passed = [true, false, true, false];
+const mixedArray = [1, "Alice", true, null];
+
+// !! for of loop to print each number
+for (const number of numbers) {
+  console.log(number);
+}
